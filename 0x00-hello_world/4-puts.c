d4d9ed8822beda1ad79314bef5ef949715916c19
Rediet <redietkfle@gmail.com>
2022-12-08 02:05:22 -0800

    Print String using C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Entry point - main
+ * puts - prints out desired string
+ * return - indicates execution status (0 - Success & 1 - Failure)
+ */
+
+int main () {
+
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return 0;
+  }
