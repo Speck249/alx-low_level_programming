5e19591c682a7fe59eb0d69934d1ad53d2f1c14f
Rediet <redietkfle@gmail.com>
2022-12-08 02:10:42 -0800

    Print String with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Entry point - main 
+ * printf - prints desired string
+ * return - indicates execution status (0 - Success & 1 - Failure)
+ */
+
+int main () {
+
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+
+   return 0;
+
+}
+
