78f42a3d489b98799a43c6e468be97a1aead3d24
Rediet <redietkfle@gmail.com>
2022-12-08 02:30:50 -0800

    Print Size of Data Types (in byte), as indicated.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-
-/*
- * Entry point - main
- * printf - prints result to console
- * sizeof() - returns memory size in byte
- * return - indicates execution status (0 - Success & 1 - Failure)
+/**
+ * main - Entry point 
+ * 
+ * return 0 - Succesful Execution status
  */
 
-int main () {
+int main(void) 
+{
 
-printf(“Size of a char: %d byte(s)\n”, sizeof(char));
-printf(“Size of an int: %d byte(s)\n”, sizeof(int));
-printf(“Size of a long int: %d byte(s)\n”, sizeof(long int));
-printf(“Size of a long long int: %d byte(s)\n”, sizeof(long long int));
-printf(“Size of a float: %d bytes(s)\n”, sizeof(float));
+   printf(“Size of a char: %d byte(s)\n”, sizeof(char));
+   printf(“Size of an int: %d byte(s)\n”, sizeof(int));
+   printf(“Size of a long int: %d byte(s)\n”, sizeof(long int));
+   printf(“Size of a long long int: %d byte(s)\n”, sizeof(long long int));
+   printf(“Size of a float: %d bytes(s)\n”, sizeof(float));
 
-return 0;
+return (0);
 }
 
