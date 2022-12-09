f8430c571f941d0222ffca2d8fe61d51ffbe478f
Rediet <redietkfle@gmail.com>
2022-12-09 01:35:50 -0800

    Prints Alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - Prints the Alphabet excluding q and e
+ *
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+for(i=97;i<=122;i++)
+{
+if(i!=101 && i!=113)
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
