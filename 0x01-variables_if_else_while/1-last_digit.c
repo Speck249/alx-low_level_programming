3e9d4ee5248163375a0f5ce38725f3cfd9b4e094
Rediet <redietkfle@gmail.com>
2022-12-09 01:18:32 -0800

    Indicates last digit as Pos., Zero, or Neg.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
-
 #include <time.h>
-
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+*main - Returns a number as Negative, Zero or Positive
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
-
 {
-
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					/* your code goes there */
-
-					return (0);
-
-}:wq
-
+int n,ld;
+srand(time(0));
+n=rand() - RAND_MAX / 2;
+if(ld>5)
+{
+printf("Last digit of %d is %d and is greater than 5\n",n,ld);
+}
+else if(ld==0)
+{
+printf("Last digit of %d is %d and is 0\n",n,ld);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n",n,ld);
+}
+return (0);
+}
