2c9791485b1f0af2e121d3fc01e3df3b396a5d0e
Rediet <redietkfle@gmail.com>
2022-12-09 01:28:24 -0800

    Printing the Alphabet in lowercase then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Prints the Alphabet in lowercase then uppercase
+ *
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char a;
+for(a='a';a<='z';a++)
+putchar(a);
+
+for(a='A';a<='Z';a++)
+putchar(a);
+return (0);
+}
