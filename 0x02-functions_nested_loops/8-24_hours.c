17f6b37fcdd6019677c3e02905b55875a30c4148
Rediet <redietkfle@gmail.com>
2022-12-13 09:56:13 -0800

    Function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -31,14 +31,8 @@ _putchar(m + 48);
 else if (m > 10)
 {
 _putchar(58);
-_putchar(48);
 _putchar((m / 10) + 48);
 _putchar((m % 10) + 48);
-}
-_putchar(n);
-_putchar(58);
-_putchar(m);
-}
 }
 _putchar('\n');
 }
