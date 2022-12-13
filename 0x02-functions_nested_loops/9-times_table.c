1d292b875573efd004a86bf660a026df18e14b7d
Rediet <redietkfle@gmail.com>
2022-12-13 03:09:41 -0800

    Function that prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*times_table -  function that prints the 9 times table, starting with 0
+*
+*Return: Always 0 (Success)
+*/
+
+void times_table(void)
+{
+int i, j, n;
+
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 10; i++)
+_putchar(n);
