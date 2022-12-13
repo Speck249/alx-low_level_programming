42162265db62f2fb987267f5fb92ec2d401383e4
Rediet <redietkfle@gmail.com>
2022-12-13 07:27:26 -0800

    function that prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*print_to_98 - prints all natural numbers from n to 98
+*
+*@n - natural numbers
+*
+*Return: Always 0 (Success)
+*/
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%d, ", n);
+}
+}
+else if (n > 98)
+{
+for (; n > 98; n--)
+{
+printf("%d, " n);
+}
+}
+printf ("\n");
+return (0);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*jack_bauer - function prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+*jack_bauer - function prints every minute of the day
 *
 *Return: Always 0 (Success)
 */
 
 void jack_bauer(void)
 {
-int n,m;
+int n, m;
 for (n = 0; n < 24; n++)
 {
 for (m = 0; m < 60; m++)
