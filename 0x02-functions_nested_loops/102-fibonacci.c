91f96b9ac1dbeb4b49ab080527db5932b771ba9a
Rediet <redietkfle@gmail.com>
2022-12-13 09:39:43 -0800

    Prints the first 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,12 +14,13 @@ n1 = 1;
 n2 = 2;
 printf("%ld, %ld", n1, n2);
 
-for (i = 0; i < 49; i++)
+for (i = 0; i < 48; i++)
 {
 n3 = n1 + n2;
 printf(", %ld", n3);
 n1 = n2;
 n2 = n3;
 }
+_putchar('\n');
 return (0);
 }
