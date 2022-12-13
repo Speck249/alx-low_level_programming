65208dd2431d428cef50cdebbf8302067f9ace83
Rediet <redietkfle@gmail.com>
2022-12-13 10:05:41 -0800

    Function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@
 void jack_bauer(void)
 {
 int n, m;
-for (n = 0; n <= 24; n++)
+for (n = 0; n < 24; n++)
 {
-for (m = 0; m <= 60; m++)
+for (m = 0; m < 60; m++)
 {
 if (n < 10)
 {
@@ -22,7 +22,7 @@ else if (n >= 10)
 _putchar((n / 10) + 48);
 _putchar((n % 10) + 48);
 }
-if (m < 10)
+if (m <= 10)
 {
 _putchar(58);
 _putchar(48);
