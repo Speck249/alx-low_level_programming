155e9e8effe096e1e3b9347987539100352dcb0d
Rediet <redietkfle@gmail.com>
2022-12-13 09:23:25 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,15 @@
 
 int main(void)
 {
-int n1 = 1, n2 = 2, n3, i;
-printf("%d, %d\n", n1, n2);
+int n1, n2, n3, i;
+n1 = 1;
+n2 = 2;
+printf("%d, %d", n1, n2);
 
-for (i = 3; i <= 50; ++i)
+for (i = 0; i <= 50; i++)
 {
 n3 = n1 + n2;
-printf ("%d, ", n3);
+printf (", %d", n3);
 n1 = n2;
 n2 = n3;
 }
