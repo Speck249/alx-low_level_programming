b26f678bbba85c4f3faa0db18859034e112c9669
Rediet <redietkfle@gmail.com>
2022-12-15 08:51:21 -0800

    Function finds the largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 *main - finds the largest prime factor
 *Return: Always 0 (Success)
@@ -13,8 +14,10 @@ a = 612852475143;
 for (b = 2; b <= a; b++)
 {
 if (a % b == 0)
+{
 a = a / b;
 b--;
+}
 }
 printf("%ld\n", b);
 return (0);
