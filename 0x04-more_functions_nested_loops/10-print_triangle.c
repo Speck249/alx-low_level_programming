4a8d5050e92b43b8e18fe5579e0576dc4c623065
Rediet <redietkfle@gmail.com>
2022-12-15 06:34:11 -0800

    Function draws triangle on terminal

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,12 +2,18 @@
 /**
 *print_triangle - function prints a triangle
 *@size: parameter
-*Return: Always 0 (Success)
+*Return: returns no value
 */
 
 void print_triangle(int size)
 {
 int i, j, k;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (i = 0; i <= size; i++)
 {
 for (j = i; j <= size; j++)
@@ -16,8 +22,9 @@ _putchar(32);
 }
 for (k = 0; k <= i; k++)
 {
-_putchar(35);
+_putchar('#');
 }
 _putchar('\n');
+}
 }
 }
