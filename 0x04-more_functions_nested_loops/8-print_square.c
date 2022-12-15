bcca18f72a62390e4dbf986df4b83740ee6744b8
Rediet <redietkfle@gmail.com>
2022-12-15 06:27:06 -0800

    Function draws square on terminal

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,19 +2,26 @@
 /**
 *print_square - function prints a square
 *@size: parameter
-*Return: Always 0 (Success)
+*Return: returns no value;
 */
 
 void print_square(int size)
 {
 int i, j;
 
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-_putchar(35);
+_putchar('#');
 }
 _putchar('\n');
+}
 }
 }
