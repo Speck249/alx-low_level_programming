53c9dd45930672b7f31f099fcfd6d51efd9de595
Rediet <redietkfle@gmail.com>
2022-12-15 08:56:51 -0800

    The FizzBuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,16 +16,12 @@ for (i = 1; i <= 100; i++)
 {
 if (i == 100)
 printf("%s", b);
-
 else if ((i % 3 == 0) && (i % 5 == 0))
 printf("%s ", c);
-
 else if (i % 3 == 0)
 printf("%s ", a);
-
 else if (i % 5 == 0)
 printf("%s ", b);
-
 else
 printf("%d ", i);
 }
