ba3f1fc26aca9d345740f43df2e50245d24e2d1d
Rediet <redietkfle@gmail.com>
2022-12-19 04:52:56 -0800

    Copy string pointed to by src to buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strcpy - copies the string pointed to by src
+*@dest: parameter
+*@src: parameter
+*Return: Always 0 (Success)
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (i >= 0)
+{
+*(dest + i) = *(src + i)
+if (*(src + i) == '\0')
+break;
+i++;
+}
+return (dest);
