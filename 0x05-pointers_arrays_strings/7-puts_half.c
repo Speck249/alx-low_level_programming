1d44772184db148dbb8fca38f9cefb7dbd190444
Rediet <redietkfle@gmail.com>
2022-12-19 02:59:32 -0800

    Print last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+*puts_half - prints half of a string
+*@str: parameter
+*Return: has no return value
+*/
+
+void puts_half(char *str)
+{
+int i, n, j;
+
+j = strlen(str);
+
+if (j % 2 == 1)
+{
+n = (j - 1) / 2;
+}
+else 
+{
+n = j / 2;
+}
+
+for (i = n; i < j; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
