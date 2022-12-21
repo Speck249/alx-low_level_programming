ff56ce6c590d966d86ffd90291adf6be36b84bce
Rediet <redietkfle@gmail.com>
2022-12-21 05:35:45 -0800

    Print the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,30 +8,34 @@
 void print_times_table(int n)
 {
 int i, j, k;
+
 if (n >= 0 && n<= 15)
 {
 for (i = 0; i <= n; i++)
 {
 _putchar(48);
-for (j = 0; j <= n; j++)
+for (j = 1; j <= n; j++)
 {
+k = i * j;
 _putchar(44);
 _putchar(32);
-k = i * j;
+
 if (k <= 9)
 _putchar(32);
+_putchar(32);
+_putchar(k + 48);
+
 if (k <= 99)
 _putchar(32);
+_putchar((k / 10) + 48);
+_putchar((k % 10) + 48);
+
 if (k >= 100)
 {
-_putchar((k / 100) + 48);
-_putchar((k / 10) % 10 + 48);
-}
-else if (k <= 99 && k >= 10)
-{
-_putchar((k / 10) + 48);
-}
+_putchar(((k / 100) % 10) + 48);
+_putchar(((k / 10) % 10) + 48);
 _putchar((k % 10) + 48);
+}
 }
 _putchar('\n');
 }
