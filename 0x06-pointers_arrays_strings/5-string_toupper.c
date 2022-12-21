6bca2e48add476a1af70a9457f3b15b3a3d35061
Rediet <redietkfle@gmail.com>
2022-12-21 02:55:15 -0800

    Convert lowercase strings to UPPERCASE

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase
+*@s: parameter;
+*Return: Always 0 (Success)
+*/
+
+char *string_toupper(char *s)
+{
+int i;
+
+while (s[i] != '\0')
+{
+if (s[i] >= 97 && s[i] <= 122)
+s[i] = s[i] - 32;
+i++;
+}
+return (s);
+}
