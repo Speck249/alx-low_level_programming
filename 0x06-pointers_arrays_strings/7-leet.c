2646c906d56d7b1f9c00933d5405d991aa94a6fb
Rediet <redietkfle@gmail.com>
2022-12-21 23:28:16 -0800

    Encode a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,9 +9,9 @@ char *leet(char *s)
 {
 int i = 0;
 int j;
-int n = 0;
-str[5] = {65, 69, 79, 64, 76};
-rep[5] = {52, 51, 48, 55, 49};
+int n = 5;
+char str[5] = {65, 69, 79, 64, 76};
+char rep[5] = {52, 51, 48, 55, 49};
 
 while (s[i])
 {
@@ -19,8 +19,8 @@ j = 0;
 
 while (j < n)
 {
-if (x[i] == str[j] || x[i] - 32 == str[j])
-x[i] = rep[j];
+if (s[i] == str[j] || s[i] - 32 == str[j])
+s[i] = rep[j];
 j++;
 }
 i++;
