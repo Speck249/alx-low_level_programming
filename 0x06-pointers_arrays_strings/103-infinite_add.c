1813210417fc03a45d313c3ad3b56acd235b0bf9
Rediet <redietkfle@gmail.com>
2022-12-22 07:09:10 -0800

    Add two numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*infinite_add - adds two numbers
+*@n1: first parameter
+*@n2: second parameter
+*@r: third parameter
+*@size_r: fourth parameter
+*Return: Always 0 (Success)
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+r = 0;
