965c444edf620b50b6b300facaa57f02169c0bb9
Rediet <redietkfle@gmail.com>
2022-12-21 23:18:31 -0800

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
+while (*s1 == *s2)
 {
-if (s1[i] != s2[i])
-{
-j = 1;
-break;
-}
-i++;
+*s1++;
+*s2++;
 }
 
-if (j == 0)
-return (0);
-else
-return (1);
+return (*s1 - *s2);
 }
