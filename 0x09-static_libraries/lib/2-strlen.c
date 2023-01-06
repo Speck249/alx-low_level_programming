8683f1cd550f9f7a4d8d48e3ee20ac512c60daac
Rediet <redietkfle@gmail.com>
2023-01-05 22:07:46 -0800

    Library File definition

---
+++ 0x09-static_libraries/lib/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strlen - returns the length of a string
+*@s: parameter
+*Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*(s + i) != '\0')
+i++;
+return (i);
+}
