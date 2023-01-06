5f85379c494b9997259cdfa8965e0b2691b372ee
Rediet <redietkfle@gmail.com>
2023-01-06 04:37:20 -0800

    Multiply two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 int a, b, Prod;
 
-if (argc >= 2)
+if (argc == 2)
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
@@ -20,7 +20,7 @@ Prod = a * b;
 printf("%d\n", Prod);
 }
 
-else if (argc < 2)
+else
 {
 printf("Error\n");
 return (1);
