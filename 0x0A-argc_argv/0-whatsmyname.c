b57755516ecd043ffebb32881a3d8223b1a09b23
Rediet <redietkfle@gmail.com>
2023-01-06 03:31:15 -0800

    Print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
 *Return: returns program name
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-if (argc > 0)
 printf("%s\n", argv[0]);
 return (0);
 }
