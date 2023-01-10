c1dbe3395e3ae8b2db436a3ecd456ce30559d18d
Rediet <redietkfle@gmail.com>
2023-01-10 07:25:29 -0800

    Create and initialize an array of char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*create_array - function creates and intializes a char array
+*@c:first parameter
+*@size: second parameter
+*Returns: Always 0 (Success)
+*/
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *a;
+
+if (size == 0)
+return (NULL);
+
+a = malloc(sizeof(c) * size);
+
+if (a == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+a[i] = c;
+
+return (a);
+}
