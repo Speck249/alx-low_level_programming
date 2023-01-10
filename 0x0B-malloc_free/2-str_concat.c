a0a872a24d5d9679b29ebb4f917bd87570aa77ba
Rediet <redietkfle@gmail.com>
2023-01-10 09:10:32 -0800

    Concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*str_concat - function concatenates two strings
+*@s1:first parameter
+*@s2: second parameter
+*Returns: Always 0 (Success)
+*/
+
+char *str_concat(char *s1, char *s2)
+{
