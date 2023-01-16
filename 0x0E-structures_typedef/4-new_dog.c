4d7a68dc223c7d17000cbc8e2200489a6bfa0e77
Rediet <redietkfle@gmail.com>
2023-01-16 11:36:55 -0800

    Define new Struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+*new_dog - Create new struct
+*@name: first parameter
+*@age: second parameter
+*@owner: third parameter
+*Return: returns no value
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+int i, j, k;
+dog_t *d_t;
+
+d_t = malloc(sizeof(*d_t));
+
+if (d_t == NULL || name == NULL || owner == NULL)
+{
+return (NULL);
+}
+
+for (j = 0; name[j]; j++)
+	;
+for (k = 0; owner[k]; k++)
+	;
+
+d_t->name = malloc(j + 1);
+d_t->owner = malloc(k + 1);
+
+if (d_t->name == NULL || d_t->owner == NULL)
+{
+free(d_t->name);
+free(d_t->owner)
+free(d_t);
+return (NULL);
+}
+
+for (i = 0; i < j; i++)
+d_t->name[i] = name[i];
+d_t->name[i] = '\0';
+d_t->age = age;
+
+for (i = 0; i < k; i++)
+d_t->owner[i] = owner[i];
+d_t->owner[i] = '\0';
+
+return (d_t);
+}
