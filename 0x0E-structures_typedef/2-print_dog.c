d8495bfcf4e27e0d011b015236a48c3bb29855a0
Rediet <redietkfle@gmail.com>
2023-01-16 05:02:15 -0800

    Print Struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+/**
+*print_dog - prints struct dog
+*@d: parameter
+*Return: no return value
+*/
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+
+else
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+
+if (d->age == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("Age: %f\n", d->age);
+}
+
+if (d->owner == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("Owner: %d\n", d->owner);
+}
+}
+}
