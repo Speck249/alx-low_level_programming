153ff9d604aa1be07061cf71ce1b2f3143777498
Rediet <redietkfle@gmail.com>
2023-01-18 07:53:56 -0800

    Operation function definition

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "cal.h"
+/**
+*op_add - function returns sum
+*@a: first parameter
+*@b: second parameter
+*Return: Always 0 (Success)
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+*op_sub - function returns difference
+*@a: first parameter
+*@b: second parameter
+*Return: Always 0 (Success)
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul - function returns product
+*@a: first parameter
+*@b: second parameter
+*Return: Always 0 (Success)
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+*op_div - function returns division
+*@a: first parameter
+*@b: second parameter
+*Return: Always 0 (Success)
+*/
+int op_div(int a, int b)
+{
+{
+if (b == 0)
+printf("Error");
+exit(98);
+}
+return (a / b);
+}
+
+/**
+*op_mod - function returns remainder
+*@a: first parameter
+*@b: second parameter
+*Return: Always 0 (Success)
+*/
+int op_mod(int a, int b)
+{
+{
+if (b == 0)
+printf("Error");
+exit(98);
+}
+return (a % b);
+}
