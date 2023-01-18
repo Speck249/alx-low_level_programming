6b556b58e70e0ec81393976a26eef5edadc7512a
Rediet <redietkfle@gmail.com>
2023-01-18 08:54:46 -0800

    Operations function

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,10 +41,10 @@ return (a * b);
 */
 int op_div(int a, int b)
 {
-{
 if (b == 0)
-printf("Error");
-exit(98);
+{
+printf("Error\n");
+exit(100);
 }
 return (a / b);
 }
@@ -57,10 +57,10 @@ return (a / b);
 */
 int op_mod(int a, int b)
 {
-{
 if (b == 0)
-printf("Error");
-exit(98);
+{
+printf("Error\n");
+exit(100);
 }
 return (a % b);
 }
