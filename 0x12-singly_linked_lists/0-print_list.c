e71afb3c63c43f4bc8b32fc279f0ef38ab18eec2
Rediet <redietkfle@gmail.com>
2023-01-26 07:54:40 -0800

    Print all the elements of list_t

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+*print_list - function prints elements of list_t
+*@h: first parameter
+*Return: Always 0 (success)
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t node = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+
+else
+printf("[%d] %s\n", h->len, h->str);
+
+node++;
+h = h->next;
+}
+
+return (node);
+}
