438579c7fdc0a550cacf16cc0953bf1ad4dfd39a
Rediet <redietkfle@gmail.com>
2023-01-31 02:22:56 -0800

    Free listint_t & set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*free_listint2 - function frees listint_t
+*@head: first parameter
+*Return: returns no value
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+
+head == NULL;
+}
