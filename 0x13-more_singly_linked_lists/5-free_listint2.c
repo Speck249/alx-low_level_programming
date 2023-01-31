cf551babb6fee56d44b30228c865b1a36483fd77
Rediet <redietkfle@gmail.com>
2023-01-31 04:23:13 -0800

    Free listint_t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,17 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
+listint_t *temp, *n_temp;
 
-if (head == NULL)
-return;
-
-while (*head != NULL)
+if (head != NULL)
 {
-temp = (*head)->next;
-free(*head);
-*head = temp;
+n_temp = *head;
+while ((temp == n_temp) != NULL)
+{
+n_temp = n_temp->next;
+free(temp);
 }
 
-head == NULL;
+*head == NULL;
+}
 }
