f25a30f24ee383e18c0671f8a8f154b1abc6d3e4
Rediet <redietkfle@gmail.com>
2023-01-31 03:45:11 -0800

    Iwqnsert a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new, *copy = *head;
-unsigned int node = 0;
+unsigned int node;
 
 new = malloc(sizeof(listint_t));
 
