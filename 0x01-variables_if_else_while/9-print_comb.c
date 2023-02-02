a8f66696acdde109f73f2cac93be1046158e3db0
Rediet <redietkfle@gmail.com>
2023-02-02 02:25:24 -0800

    Print all possible combinations of Base 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include <string.h>
 /**
-*main - prints all possible combinations of single-digit numbers
-*
+*main - prints all possible combinations of Base 10
 *Return: Always 0 (Success)
 */
 
 int main(void)
 {
 int i;
-for (i = 48; i <= 57; i++)
+
+for (i = 48; i < 58; i++)
 {
 putchar(i);
-{
 if (i < 57)
 {
 putchar(44);
 putchar(32);
 }
 }
-}
 putchar('\n');
+
 return (0);
 }
