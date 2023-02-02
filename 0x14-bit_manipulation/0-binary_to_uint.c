2cf692dd29b6310b4f12e8af9296cec2f26543ac
Rediet <redietkfle@gmail.com>
2023-02-02 08:56:39 -0800

    Convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,14 +13,13 @@ unsigned int result = 0;
 if (!b)
 return (0);
 
-while (b[i])
+for (i = 0; b[i]; i++)
 {
-if (b[i] != 0 || b[i] != 1)
+if (b[i] != 0 && b[i] != 1)
 {
 return (0);
 }
 result = 2 * result + (b[i] - 48);
-i++;
 }
 
 return (result);
