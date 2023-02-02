5be7209554eecaad552ce3a09473377eb5c1fc66
Rediet <redietkfle@gmail.com>
2023-02-02 02:40:10 -0800

    Print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
-*print_sign - function prints the sign of a number
-*
-*@n: integer to be evaluated
-*
+*print_sign - Prints sign of a number
+*@n: parameter
 *Return: Always 0 (Success)
 */
 
@@ -14,11 +12,13 @@ if (n > 0)
 _putchar(43);
 return (1);
 }
+
 else if (n == 0)
 {
 _putchar(48);
 return (0);
 }
+
 else
 {
 _putchar(45);
