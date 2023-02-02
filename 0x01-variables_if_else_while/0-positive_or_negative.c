e580bdd3ac2bd3af3c9e5a9bd2f48a5b4722d187
Rediet <redietkfle@gmail.com>
2023-02-02 01:48:47 -0800

    Random number generator

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
-*main - Prints whether number is neg. or pos.
+*main - Generates random numbers.
 *Return: Always 0 (Success)
 */
 
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -23,5 +25,6 @@ else
 {
 printf("%d is negative\n", n);
 }
+
 return (0);
 }
