7c07f20d2442c077d12256a6e18f44fb8eb9c6ab
Rediet <redietkfle@gmail.com>
2023-02-02 08:19:37 -0800

    Return sum of integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
-*add - adds two integers and returns the result
-*
-*@a: integer value
-*
-*@b: integer value
-*
+*add - adds two integers
+*@a:first parameter
+*@b: second parameter
 *Return: Always 0 (Success);
 */
 
 int add(int a, int b)
 {
 int sum;
+
 sum = a + b;
 return (sum);
 }
