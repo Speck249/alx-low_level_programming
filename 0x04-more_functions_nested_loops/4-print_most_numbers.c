0f95285da6279dcb20311d1eb3209a5bba1cbf3e
Rediet <redietkfle@gmail.com>
2023-02-02 23:53:26 -0800

    Print Base 10 without 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-*print_most_numbers - function prints the numbers, from 0 to 9
-*Return: Always 0 (Success)
+*print_most_numbers - prints Base 10 except 2 & 4
+*Return: Base 10 without 2 & 4
 */
 
 void print_most_numbers(void)
 {
 int i;
-for (i = 48; i <= 57; i++)
+
+for (i = 48; i < 58; i++)
 {
 if (i != 50 && i != 52)
+{
 _putchar(i);
 }
 _putchar('\n');
