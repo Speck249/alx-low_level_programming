ef9584706866b37f19c8bb5328c0fe44e1e43296
Rediet <redietkfle@gmail.com>
2023-02-02 23:51:59 -0800

    Print Base 10

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-*print_numbers - function prints the numbers, from 0 to 9
+*print_numbers - prints Base 10
 *Return: no return
 */
 
 void print_numbers(void)
 {
 int i;
-for (i = 48; i <= 57; i++)
+
+for (i = 48; i < 58; i++)
 {
 _putchar(i);
 }
