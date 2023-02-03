3b36fb33cdf156cdb13dd9fc51b2ef57c2849b04
Rediet <redietkfle@gmail.com>
2023-02-03 00:19:58 -0800

    Print line to terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_line - function draws a straight line in the terminal
+*print_line - prints line to terminal
 *@n: parameter
 *Return: returns no value
 */
@@ -8,14 +8,14 @@
 void print_line(int n)
 {
 int i;
+
 if (n <= 0)
-{
 _putchar('\n');
-}
+
 else
-{
 for (i = 0; i < n; i++)
+{
 _putchar(95);
-_putchar('\n');
 }
+_putchar('\n');
 }
