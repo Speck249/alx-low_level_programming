060aba191bce08e4594dc0cbb2bb27289ba97075
Rediet <redietkfle@gmail.com>
2023-02-06 11:45:09 -0800

    Read & print to Stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,10 +9,16 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t rd, wr, op;
+ssize_t rd, wr;
+int op;
 char *buff;
 
 if (filename == NULL)
+return (0);
+
+op = open(filename, O_RDONLY);
+
+if (op == -1)
 return (0);
 
 buff = malloc(sizeof(char) * letters);
@@ -21,15 +27,8 @@ return (0);
 
 rd = read(op, buff, letters);
 wr = write(STDOUT_FILENO, buff, rd);
-op = open(filename, O_RDONLY);
 
-if (rd == -1 || wr == -1 || op == -1 || wr != rd)
-{
+close (op);
 free(buff);
-return (0);
-}
-
-free(buff);
-close(op);
 return (wr);
 }
