348836e72b4bd54d3627a3f3d6d838091197d2e6
Rediet <redietkfle@gmail.com>
2023-06-28 05:48:14 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*swap_int - swaps the values of two integers
-*@a: parameter
-*@b: parameter
+*swap_int - swaps values of two integers
+*@a: first parameter
+*@b: second parameter
 *Return: no return value
 */
 
